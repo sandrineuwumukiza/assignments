2e2e392b60edeb3f57856de0819513cd6271c7a7
Your Name <uwasandrine99@gmail.com>
2023-04-11 10:34:24 +0200

    change

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const string="now we are learning javascript";
-const result = string.split("").reverse().join("");
-console.log(result);
+//const string="now we are learning javascript";
+//const result = string.split("").reverse().join("");
+//console.log(result);
+//question one
+//dispay string in reverse
+let string="Now we are learning javascript"
+function reverse(){
+    return string.split("").reverse().join()
+}
+console.log(reverse);
+// question two
+//let n=[3,5,6,7,10,11,19,20,33]
+//let solution = n.filter(num=>num>10);
+//console.log(solution);
 
-let n=[3,5,6,7,10,11,19,20,33]
-let solution = n.filter(num=>num>10);
-console.log(solution);
+//const str = "I like this program very much";
+//const output = str.split(" ").reverse().join(" .");
+//console.log(output);
 
-const str = "I like this program very much";
-const output = str.split(" ").reverse().join(" .");
-console.log(output);
+//const stri = "Hello world";
+//const out = stri.split(" ").reverse().join(" .");
+//console.log(out);
 
-const str = "Hello world";
-const result = str.split(" ").reverse().join(" .");
-console.log(result);
-
-function isPolindrome(str){
-    str=str.toLowerCase().replace(/[^a-z 0-9]/g,'');
-    return str=str.toLowerCase().reverse().join(" ");
- }
- console.log(isPolindrome('radar'));
- console.log(isPolindrome('level'));
- console.log(isPolindrome('12321'));
- console.log(isPolindrome('tomorrow'));
+//function isPalindrome(str){
+   // str=str.toLowerCase().replace(/[^a-z 0-9]/g,'');
+    //return str.toLowerCase().reverse().join(" ");
+ //}
+// console.log(isPalindrome('radar'));
+ //console.log(isPalindrome('level'));
+ //console.log(isPolindrome('12321'));
+ //console.log(isPolindrome('tomorrow'));
